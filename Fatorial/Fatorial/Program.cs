01c441f08397081dcde453c4d69f4e197ad81f89
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2024-08-28 22:14:15 -0300

    Add files via upload

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            double numero = LerNumero();
+
+            double fatorial = CalcularFatorial(numero);
+
+            Console.WriteLine($"{numero}! = {fatorial}");
+
+            Console.ReadKey();
+        }
+
+        private static double CalcularFatorial(double numero)
+        {
+            double fatorial = numero;
+
+            for (double i = numero - 1; i >= 1; i--)
+            {
+                fatorial *= i;
+            }
+
+            return fatorial;
+        }
+
+        private static double LerNumero()
+        {
+            bool ok = false;
+            double numero = 0;
+
+            do
+            {
+                try
+                {
+                    numero = double.Parse(Console.ReadLine());
+                    ok = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nValor inválido!\n");
+                }
+            } while (ok != true);
+
+            return numero;
+        }
+    }
+}
