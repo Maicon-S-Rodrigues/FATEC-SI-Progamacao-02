1d59842fbfbf1e85a2e4da3cf4e337704fb36e36
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2024-08-28 21:46:42 -0300

    Add files via upload

---
+++ CalculoMedia/CalculoMedia/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace CalculoMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float notaUm = 0, notaDois = 0, notaTres = 0, media = 0;
+
+            Console.Write("Digite a primeira Nota: ");
+            notaUm = LerNota();
+
+            Console.Write("\nDigite a segunda Nota: ");
+            notaDois = LerNota();
+
+            Console.Write("\nDigite a terceira Nota: ");
+            notaTres = LerNota();
+
+            media = CalcularMedia(notaUm, notaDois, notaTres);
+
+            InformarResultado(media);
+        }
+
+        private static float LerNota()
+        {
+            bool ok = false;
+            float nota = 0;
+
+            do
+            {
+                try
+                {
+                    nota = float.Parse(Console.ReadLine());
+                    
+                    if (nota >= 0 && nota <= 10) 
+                    {
+                        ok = true;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nValor inválido!\nInforme um valor entre 0 e 10!\n");
+                }
+            } while (ok != true);
+
+            return nota;
+        }
+
+        private static float CalcularMedia(
+            float notaUm,
+            float notaDois, 
+            float notaTres
+        ) =>
+            (notaUm + notaDois + notaTres) / 3;
+        
+
+        private static void InformarResultado(float media)
+        {
+            if (media < 3) 
+            {
+                Console.WriteLine("Reprovado, com Media: " + media);
+            }
+            else if (media < 6)
+            {
+                Console.WriteLine("Exame, com Media: " + media);
+            }
+            else
+            {
+                Console.WriteLine("Aprovado, com Media: " + media);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NumeroPrimo/NumeroPrimo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace NumeroPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            bool primo = false;
+
+            Console.WriteLine("Digite um numero: ");
+            numero = LerNumero();
+
+            primo = IsPrimo(numero);
+
+            switch (primo)
+            {
+                case true:
+                    Console.WriteLine($"O numero {numero} é Primo!");
+                    break;
+
+                case false:
+                    Console.WriteLine($"O numero {numero} não é Primo!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool IsPrimo(int numero)
+        {
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static int LerNumero()
+        {
+            bool ok = false;
+            int numero = 0;
+
+            do
+            {
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+
+                    if (numero == 1)
+                    {
+                        Console.WriteLine("\nO numero 1 não é Primo!\nInforme outro...\n");         
+                    }
+                    else
+                    {
+                        ok = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nValor inválido!\nInforme um numero inteiro!\n");
+                }
+            } while (ok != true);
+
+            return numero;
+        }
+    }
+}
