699b868bd51a7e2bdc078f808d854e15c8d6afba
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2024-09-04 22:15:50 -0300

    Add files via upload

---
+++ ObjectOrientationExample/ObjectOrientationExample/Person.cs
@@ -0,0 +1,65 @@
+﻿namespace ObjectOrientationExample
+{
+    public class Person
+    {
+        public Person(
+            string name,
+            char sex,
+            int age
+        )
+        {
+            Name = name;
+            Sex = sex;
+            Age = age;
+        }
+
+        public Person()
+        {
+            Name = "Sem nome";
+            Sex = ' ';
+            Age = 0;
+        }
+
+        public string Name;
+        public char Sex;
+        public int Age;
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public void SetSex(char sex)
+        {
+            Sex = sex;
+        }
+
+        public char GetSex()
+        {
+            return Sex;
+        }
+
+        public override string ToString()
+        {
+            return $"\nDados da pessoa: " +
+                   $"\nNome: {Name}" +
+                   $"\nIdade: {Age}" +
+                   $"\nSexo: {Sex}" + "\n";
+        }
+    }
+}
---
+++ ObjectOrientationExample/ObjectOrientationExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ObjectOrientationExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.ToString());
+
+            string name;
+            int age;
+            char sex;
+
+            Console.Write("Informe o nome da pessoa: ");
+            name = Console.ReadLine();
+
+            Console.Write("Informe a idade da pessoa: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o sexo da pessoa (F/M): ");
+            string aux = Console.ReadLine();
+            sex = aux.ToUpper()[0];
+
+            person = new Person(name, sex, age);
+
+            Console.WriteLine(person.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
