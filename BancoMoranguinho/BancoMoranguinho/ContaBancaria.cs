efbaee355240e890391fa23ade63c8b6f062f911
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2024-09-11 22:06:53 -0300

    Add files via upload

---
+++ BancoMoranguinho/BancoMoranguinho/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BancoMoranguinho
+{
+    internal class ContaBancaria
+    {
+        public ContaBancaria()
+        {
+            NumeroConta = GerarNumeroConta();
+            Saldo = 0;
+        }
+
+        private string NumeroConta;
+        private decimal Saldo;
+
+        public bool Depositar(decimal valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Sacar(decimal valor)
+        {
+            if (Saldo > 0 || valor <= Saldo)
+            {
+                Saldo -= valor;
+                return true;
+            }
+
+            return false;
+        }
+
+        public decimal VerSaldo()
+        {
+            return Saldo;
+        }
+
+        private string GerarNumeroConta()
+        {
+            int numero = 1;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                numero *= (new Random().Next(1, 10));
+            }
+
+            return numero.ToString();
+        }
+    }
+}
---
+++ BancoMoranguinho/BancoMoranguinho/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace BancoMoranguinho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+
+            Menu(conta);
+
+            Console.ReadKey();
+        }
+
+        private static void Menu(ContaBancaria conta)
+        {
+            bool continuar = true;
+            decimal valor = 0;
+            do
+            {
+                Console.WriteLine("Escolha sua opção:");
+                Console.WriteLine("1 - Depositar\n2 - Sacar\n3 - Ver Saldo\n0 - Sair");
+                int opcao = int.Parse(Console.ReadLine());
+                
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Informe o valor desejado para depositar: ");
+                        valor = LerValor("depositar");
+
+                        if (conta.Depositar(valor))
+                        {
+                            Console.WriteLine("Depósito realizado!");
+                            MensagemContinuar();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valor inválido para depósito...");
+                            MensagemContinuar();
+                        }
+                        break;
+
+                    case 2:
+                        Console.Write("Informe o valor desejado para sacar: ");
+                        valor = LerValor("sacar");
+
+                        if (conta.Sacar(valor))
+                        {
+                            Console.WriteLine("Saque realizado!");
+                            MensagemContinuar();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saldo insuficiente para o saque solicitado...");
+                            MensagemContinuar();
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Seu saldo atual é: " + conta.VerSaldo());
+                        MensagemContinuar();
+                        break;
+
+                    case 0: continuar = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!\n");
+                        MensagemContinuar();
+                        break;
+                }
+
+            } while (continuar);
+        }
+
+        private static decimal LerValor(string acao)
+        {
+            do
+            {
+                try
+                {
+                    return decimal.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Valor inválido...");
+                    MensagemContinuar();
+                    Console.Write($"Informe o valor desejado para {acao}: ");
+                }
+            } while (true);
+        }
+
+        private static void MensagemContinuar()
+        {
+            Console.WriteLine("\nAperte ´Enter´ para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
