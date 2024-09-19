859945acbfb0d58b75a8839119b297e0dca33fbe
Maicon Siqueira Rodrigues <110917695+Maicon-S-Rodrigues@users.noreply.github.com>
2024-09-18 22:06:11 -0300

    Add files via upload

---
+++ ExAssociacao/ExAssociacao/PessoaFisica.cs
@@ -0,0 +1,28 @@
+﻿namespace ExAssociacao
+{
+    internal class PessoaFisica : Pessoa
+    {
+        public PessoaFisica(
+            string cpf,
+            string nome,
+            string telefone,
+            Endereco endereco,
+            int idade
+        ) : base(
+            nome,
+            telefone, 
+            endereco,
+            idade
+        )
+        {
+            CPF = cpf;
+        }
+
+        public string CPF { get; set; }
+
+        public override string ToString()
+        {
+            return $"\nCPF: {CPF},\nNome: {Nome},\nIdade: {Idade},\nTelefone: {Telefone},\n\nEndereço:{Endereco}";
+        }
+    }
+}
---
+++ ExAssociacao/ExAssociacao/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExAssociacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa(
+                "Maicon Siqueira Rodrigues",
+                "1698870003",
+                new Endereco(
+                    "Precide São Martim",
+                    "Rua",
+                    "Araraquara",
+                    "SP",
+                    "Jardim Santa Clara",
+                    14811,
+                    126,
+                    "Portaria - Fatec Araraquara"
+                ),
+                25
+            );
+
+            Console.WriteLine("Dados da Pessoa:" + pessoa.ToString());
+
+            Console.WriteLine("____________________________________________________________");
+
+            PessoaFisica pessoaFisica = new PessoaFisica(
+                "47933005588",
+                "Maicon Siqueira Rodrigues",
+                "1698870003",
+                new Endereco(
+                    "Precide São Martim",
+                    "Rua",
+                    "Araraquara",
+                    "SP",
+                    "Jardim Santa Clara",
+                    14811,
+                    126,
+                    "Portaria - Fatec Araraquara"
+                ),
+                25
+            );
+
+            Console.WriteLine("Dados da Pessoa Física:" + pessoaFisica.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
